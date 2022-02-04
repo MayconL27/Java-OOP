49b49e8af0db003eaee5be0b176cc1b2561886bd
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-02-04 15:13:00 -0300

    Primeiro commit

---
+++ ProgramacaoOrientadaObjetosDIO/MetodoExercicio/Carro.java
@@ -0,0 +1,47 @@
+package ProgramacaoOrientadaObjetosDIO.MetodoExercicio;
+
+public class Carro {
+       
+        // Atributos.
+        String cor;
+        String modelo;
+        int capacidadeTanque;
+
+        // Construtor.
+        Carro() {
+
+        }
+
+        Carro(String cor, String modelo, int capacidadeTanque) {
+            this.cor = cor;
+            this.modelo = modelo;
+            this.capacidadeTanque = capacidadeTanque;
+        }
+
+        // Gets e sets.
+        public String getCor() {
+            return cor;
+        }
+        public void setCor(String cor) {
+            this.cor = cor;
+        }
+        public String getModelo() {
+            return modelo;
+        }
+        public void setModelo(String modelo) {
+            this.modelo = modelo;
+        }
+        public int getCapacidadeTanque() {
+            return capacidadeTanque;
+        }
+        public void setCapacidadeTanque(int capacidadeTanque) {
+            this.capacidadeTanque = capacidadeTanque;
+        }
+        
+        // Métodos.
+        double totalValorTanque(double valorCombustivel) {
+            return capacidadeTanque = valorCombustivel;
+        }
+    
+    
+}
