64cecb74153b4f390e36436dc257f008d3bec73a
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-02-08 15:24:02 -0300

    Aula 4

---
+++ ProgramacaoOrientadaObjetosDIO/Interface/OperacaoMatematica.java
@@ -0,0 +1,12 @@
+/** Crie uma interface chamada OperacaoMatematica. Crie também 4 metodos 
+  * das operações básicas: soma, subtração, multiplicação e divisão. */
+
+  interface OperacaoMatematica {
+
+    // Metodos.
+      void soma(double operando1, double operando2);
+      void subtracao(double operando1, double operando2);
+      void multiplicacao(double operando1, double operando2);
+      void divisao(double operando1, double operando2);
+                
+}
