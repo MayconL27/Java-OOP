39b28af7ecde7e35ab644e6cc0994c5092a2618d
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-03-14 20:52:09 -0300

    Exer 01 e 02 incompletos

---
+++ ExerciciosClasses/Exer01.java
@@ -0,0 +1,10 @@
+public class Exer01 {
+    public static void main(String[] args) {
+        
+        /** 1.Classe Bola: Crie uma classe que modele uma bola:
+         * Atributos: Cor, circunferência, material
+         * Métodos: trocaCor e mostraCor */
+
+    }
+    
+}
---
+++ ExerciciosClasses/Exer02.java
@@ -0,0 +1,8 @@
+public class Exer02 {
+    public static void main(String[] args) {
+        /** 2. Classe Quadrado: Crie uma classe que modele um quadrado:
+         * Atributos: Tamanho do lado
+         * Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área; */
+    }
+    
+}
