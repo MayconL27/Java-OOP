1f624ba1cc7c8a5f0aa93887e3c5d39c82fa5b6d
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-03-16 20:58:48 -0300

    Exercicio 01

--- ExerciciosClasses/Exer01.java
+++ ExerciciosClasses/Exer01.java
@@ -1,10 +1,14 @@
+import model.Bola;
+
 public class Exer01 {
     public static void main(String[] args) {
         
         /** 1.Classe Bola: Crie uma classe que modele uma bola:
          * Atributos: Cor, circunferência, material
          * Métodos: trocaCor e mostraCor */
-
-    }
-    
+        Bola b = new Bola("Azul");
+        System.out.println(b.mostrarCor());
+        b.trocaCor("Preta");
+        System.out.println(b.mostrarCor());
+    }  
 }
---
+++ ExerciciosClasses/model/Bola.java
@@ -0,0 +1,45 @@
+package model;
+
+
+public class Bola {
+
+    // Atributos.
+    private String cor;
+    private int circunferencia;
+    private String material;
+
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+    public int getCircunferencia() {
+        return circunferencia;
+    }
+    public void setCircunferencia(int circunferencia) {
+        this.circunferencia = circunferencia;
+    }
+    public String getMaterial() {
+        return material;
+    }
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    // Métodos.
+
+    // Construtor:
+    public Bola(String cor) {
+        this.cor = cor;
+    }
+
+    public void trocaCor(String cor) {
+        this.cor = cor;
+    }
+    
+    public String mostrarCor() {
+        return this.cor;
+    }
+    
+}
